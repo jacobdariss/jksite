4bf7e252a01c9da9b0d97a8c9c42217381f3afaa
Jacob Dariss <jacob@jokko.africa>
2026-03-29 19:51:30 +0000

    feat: remove Blog from navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,6 @@ const NAV_LINKS = [
   { href: '/institution', label: 'Institution' },
   { href: '/services', label: 'Services' },
   { href: '/partenaires', label: 'Partenaires' },
-  { href: '/blog', label: 'Blog' },
 ]
 
 export default function Navbar() {
